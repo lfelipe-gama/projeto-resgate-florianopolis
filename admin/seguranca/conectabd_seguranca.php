9594e0e2fe0714c092a56be2f0b9f997ffc051ae
Felipe <lfelipe@websis.inf.br>
2017-10-11 17:58:56 -0300

    versão deploy ok

--- admin/seguranca/conectabd_seguranca.php
+++ admin/seguranca/conectabd_seguranca.php
@@ -1,5 +1,5 @@
 ﻿<?php
-	$conexao = mysqli_connect("mysql857.umbler.com", "user_resgate", "yasjekTac7", "proj_resgate","41890");
+	$conexao = mysqli_connect("localhost", "", "", "","");
 	if (!$conexao) {
 		echo "Não foi possível usar o banco de dados:".mysqli_error($conexao);
 		exit;
